97294c5811268eaa1339c6a44d4a8c0f398f0220
Sorin Chis <kish.sorin@yahoo.com>
2019-02-25 14:49:35 +0200

    transform Saved Movie List into Links

--- client/src/App.js
+++ client/src/App.js
@@ -23,8 +23,11 @@ export default class App extends Component {
 		return (
 			<div>
 				<SavedList list={this.state.savedList} />
-				<Route exact path="/" component={MovieList}/>
-            <Route  path="/movie/:id" component={Movie}/>
+				<Route exact path="/" component={MovieList} />
+				<Route
+					path="/movie/:id"
+					render={(props) => <Movie {...props} addToSavedList={this.addToSavedList} />}
+				/>
 			</div>
 		);
 	}
